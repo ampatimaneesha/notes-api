3668b28b10ea5cdda7be2355b0bf85c4b31bc00a
ampatimaneesha <ampatimaneesha@gmail.com>
2026-05-16 17:58:03 +0530

    added start script

--- index.js
+++ index.js
@@ -67,6 +67,12 @@ const app = express();
 
 const cors = require('cors');
 app.use(cors());
+
+
+app.use(cors({
+  origin: "https://notes-app-jipg.vercel.app",
+  credentials: true
+}));
 app.use(express.json());
 
 // ✅ IMPORT SWAGGER
@@ -125,8 +131,9 @@ app.use('/api/notes', noteRoutes);
 
 
 // ✅ START SERVER
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log("Server running");
 });
