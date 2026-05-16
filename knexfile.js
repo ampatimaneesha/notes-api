fdd158ed507eb1090952b807040963d4bc9bfddf
ampatimaneesha <ampatimaneesha@gmail.com>
2026-05-16 18:14:58 +0530

    added knex

--- knexfile.js
+++ knexfile.js
@@ -3,24 +3,17 @@
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
-module.exports = {
 
-  development: {
-    client: 'pg',
-    connection: {
-      host: '127.0.0.1',
-      port: 5432,
-      user: 'postgres',
-      password: '1234',
-      database: 'notes_db'
-    },
-    migrations: {
-      directory: './migrations'
-    }
-  }
 
   
-
- 
-
+ module.exports = {
+  development: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
 };
+   
+
