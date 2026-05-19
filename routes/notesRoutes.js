e0e9fb5c4592d9f810a91f5b3031a6446f374250
ampatimaneesha <ampatimaneesha@gmail.com>
2026-05-19 17:23:44 +0530

    fix done

--- routes/notesRoutes.js
+++ routes/notesRoutes.js
@@ -5,11 +5,21 @@ const db = require("../db");
 // GET ALL NOTES
 router.get("/", async (req, res) => {
   try {
-    const notes = await db("notes").select("*").orderBy("id", "desc");
+
+    const notes = await db("notes")
+  .where({ trash: false })
+  .select("*")
+  .orderBy("id", "desc");
+
     res.json(notes);
+
   } catch (err) {
+
     console.log(err);
-    res.status(500).json({ error: err.message });
+
+    res.status(500).json({
+      error: err.message
+    });
   }
 });
 // ADD NOTE
@@ -32,16 +42,27 @@ router.post("/add", async (req, res) => {
 });
 
 // DELETE NOTE
+// MOVE NOTE TO TRASH
 router.delete("/:id", async (req, res) => {
   try {
+
     await db("notes")
       .where({ id: req.params.id })
-      .del();
+      .update({
+        deleted: true,
+      });
 
-    res.json({ success: true });
+    res.json({
+      success: true
+    });
+
   } catch (err) {
+
     console.log(err);
-    res.status(500).json({ error: err.message });
+
+    res.status(500).json({
+      error: err.message
+    });
   }
 });
 // FAVORITE NOTE
@@ -59,6 +80,80 @@ router.put("/favorite/:id", async (req, res) => {
       .returning("*");
 
     res.json(updated[0]);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: err.message });
+  }
+});
+// UPDATE NOTE
+router.put("/:id", async (req, res) => {
+  try {
+
+    const { text } = req.body;
+
+    const updated = await db("notes")
+      .where({ id: req.params.id })
+      .update({
+        text,
+      })
+      .returning("*");
+
+    res.json(updated[0]);
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: err.message });
+  }
+});
+// GET TRASH NOTES
+router.get("/trash/all", async (req, res) => {
+  try {
+
+    const trash = await db("notes")
+      .where({ deleted: true })
+      .orderBy("id", "desc");
+
+    res.json(trash);
+
+  } catch (err) {
+
+    console.log(err);
+
+    res.status(500).json({
+      error: err.message
+    });
+  }
+});
+
+// GET TRASH NOTES
+router.get("/trash", async (req, res) => {
+  try {
+
+    const trashNotes = await db("notes")
+      .where({ trash: true })
+      .orderBy("id", "desc");
+
+    res.json(trashNotes);
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: err.message });
+  }
+});
+// MOVE NOTE TO TRASH
+// MOVE NOTE TO TRASH
+router.put("/trash/:id", async (req, res) => {
+  try {
+
+    const updated = await db("notes")
+      .where({ id: req.params.id })
+      .update({
+        trash: true,
+      })
+      .returning("*");
+
+    res.json(updated[0]);
+
   } catch (err) {
     console.log(err);
     res.status(500).json({ error: err.message });
